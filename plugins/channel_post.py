6f203d336cec1fd662f86b00ee96d06752ed835e
filestorebotbyaccbot <filestorebotbyaccbot@gmail.com>
2026-04-19 10:21:43 +0530

    Refactor channel_post for clarity and efficiency

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -1,14 +1,3 @@
-# Don't Remove Credit @CodeFlix_Bots, @rohit_1888
-# Ask Doubt on telegram @CodeflixSupport
-#
-# Copyright (C) 2025 by Codeflix-Bots@Github, < https://github.com/Codeflix-Bots >.
-#
-# This file is part of < https://github.com/Codeflix-Bots/FileStore > project,
-# and is released under the MIT License.
-# Please see < https://github.com/Codeflix-Bots/FileStore/blob/master/LICENSE >
-#
-# All rights reserved.
-#
 import asyncio
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
@@ -16,15 +5,15 @@ from pyrogram.errors import FloodWait
 from bot import Bot
 from config import *
 from helper_func import encode, admin
-from database.database import db # Database import kiya
+from database.database import db # Database import zaroori hai
 
 @Bot.on_message(filters.private & admin & ~filters.command(['start', 'commands','users','broadcast','batch', 'custom_batch', 'genlink','stats', 'dlt_time', 'check_dlt_time', 'dbroadcast', 'ban', 'unban', 'banlist', 'addchnl', 'delchnl', 'listchnl', 'fsub_mode', 'pbroadcast', 'add_admin', 'deladmin', 'admins', 'addpremium', 'premium_users', 'remove_premium', 'myplan', 'count', 'delreq']))
 async def channel_post(client: Client, message: Message):
-    # Sirf media files (Audio, Video, Document) ko handle karne ke liye
+    # Sirf Audio Stories aur Files ke liye
     if not (message.document or message.video or message.audio):
         return
 
-    reply_text = await message.reply_text("<b>Processing and Indexing... Please Wait!</b>", quote = True)
+    reply_text = await message.reply_text("<b>Processing... Indexing File to Database!</b>", quote = True)
     
     try:
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
@@ -32,30 +21,32 @@ async def channel_post(client: Client, message: Message):
         await asyncio.sleep(e.x)
         post_message = await message.copy(chat_id = client.db_channel.id, disable_notification=True)
     except Exception as e:
-        print(f"Forward Error: {e}")
+        print(f"Error: {e}")
         await reply_text.edit_text("Something went Wrong..!")
         return
 
-    # File Details nikalna search ke liye
+    # 1. File Name nikalna (Search ke liye yahi kaam aayega)
     file_name = ""
     if message.document: file_name = message.document.file_name
-    elif message.video: file_name = message.video.file_name or "Video File"
-    elif message.audio: file_name = message.audio.file_name or "Audio File"
+    elif message.video: file_name = message.video.file_name or "Video"
+    elif message.audio: file_name = message.audio.file_name or "Audio"
 
+    # 2. Link generate karna
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
     
-    # 🔥 ZAROORI: Database mein file ko save karna taaki Search kaam kare
+    # 3. 🔥 DATABASE MEIN SAVE KARNA (INDEXING) 🔥
+    # Hum 'db' variable use kar rahe hain jo 'database.py' se aa raha hai
     try:
-        # Hum wahi collection use karenge jo database.py mein banaya tha
-        await client.database['files'].update_one(
+        # Hum 'files' collection mein file_name aur base64_string save karenge
+        await db.database['files'].update_one(
             {"file_id": base64_string},
             {"$set": {"file_name": file_name, "msg_id": post_message.id}},
             upsert=True
         )
     except Exception as e:
-        print(f"DB Save Error: {e}")
+        print(f"DB Error: {e}")
 
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
@@ -67,4 +58,3 @@ async def channel_post(client: Client, message: Message):
             await post_message.edit_reply_markup(reply_markup)
         except:
             pass
-
